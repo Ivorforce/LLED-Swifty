3801b943e3dc43e58cef9088f03008347e88774b
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2021-07-25 15:13:04 +0200

    Initial Commit

---
+++ LEDTesty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LEDTesty
+//
+//  Created by Lukas Tenbrink on 25.07.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LEDTesty/LEDTestyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LEDTestyApp.swift
+//  LEDTesty
+//
+//  Created by Lukas Tenbrink on 25.07.21.
+//
+
+import SwiftUI
+
+@main
+struct LEDTestyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
